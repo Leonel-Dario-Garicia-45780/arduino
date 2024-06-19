288fedd0d1ca476539e27a4b6cfc376adf8c7513
leonel <gun45780@gmail.com>
2024-06-19 17:52:45 -0500

    una funcion pequeña, prender el pin 2

---
+++ intento_arduino_1/intento_arduino_1.ino
@@ -0,0 +1,17 @@
+void setup() {
+  // put your setup code here, to run once:
+
+  pinMode(2,OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+    // !!ensallo 1    enciende y apaga luz azul del pi 2  (toca mantener presionado el boton de BOOT  y luego se presiona el boton de EN)
+    // digitalWrite(2,HIGH);
+    // delay(3000);
+    // digitalWrite(2,LOW);
+    // delay(1000);
+
+}
