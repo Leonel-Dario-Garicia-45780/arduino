700335032c1b17b943089f91ca28d023bad7831f
leonel <gun45780@gmail.com>
2024-06-20 19:40:29 -0500

    primer condicional

--- intento_arduino_1/intento_arduino_1.ino
+++ intento_arduino_1/intento_arduino_1.ino
@@ -1,17 +1,37 @@
+int pinPulsador=4;
+
+int pinLed=2;
+
 void setup() {
   // put your setup code here, to run once:
 
-  pinMode(2,OUTPUT);
+    // !!ensallo 1    enciende y apaga luz azul del pi 2  (toca mantener presionado el boton de BOOT  y luego se presiona el boton de EN)
+      //pinMode(2,OUTPUT);
 
+    // !! ensallo 2
+    pinMode(pinPulsador, INPUT);
+    pinMode(pinLed, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+    //!! ensallo 2
+{
+    if(digitalRead(pinPulsador)== HIGH ){
+
+      digitalWrite(pinLed, HIGH); 
+    }
+    else{
+
+      digitalWrite(pinLed, LOW); 
+    }
+    delay(100);
+}
 
     // !!ensallo 1    enciende y apaga luz azul del pi 2  (toca mantener presionado el boton de BOOT  y luego se presiona el boton de EN)
-    // digitalWrite(2,HIGH);
-    // delay(3000);
-    // digitalWrite(2,LOW);
-    // delay(1000);
+      //  digitalWrite(2,HIGH);
+      //  delay(3000);
+      //  digitalWrite(2,LOW);
+      //  delay(1000);
 
 }
