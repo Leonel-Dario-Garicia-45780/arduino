21ccf388c591cae007b4ca176532730335659bb2
leonel <gun45780@gmail.com>
2024-07-03 22:24:58 -0500

    falta completar

--- intento_arduino_1/intento_arduino_1.ino
+++ intento_arduino_1/intento_arduino_1.ino
@@ -1,207 +1,42 @@
-/* // dependencias del video
-// #include <WiFi.h>
-// #include <DHT.h>
-// #include <HTTPClient.h>
-// #include <ArduinoJson.h>
-// #include "time.h"
-
-//dependencias que al parecer funcionan
 #include <WiFi.h>
-#include <DHT.h>
-#include <HTTPClient.h>
-//#include <Arduino_JSON.h>
-#include <ArduinoJson.h>
-#include "time.h"
-
-
-
-//definir pines
-#define DHTpin 27
-#define SOILPIN 32
-#define LDRpin 33
-#define LEDamarillopin 15 // respuesta positiva
-#define LEDverdepin 16    // respuesta negativa
-#define SOILPOWER 4
-
-DHT dHt(DHTpin, DHT11);
-
-unsigned long epochtime;
-unsigned long dataMillis = 0;
-
-const char* ntpserver = "pool.ntp.org";  // al parecer esto no cambia
-const char* servername = "mongodb+srv://gun45780:adso145780@cluster0.feqs5gd.mongodb.net/" //coneccion amongo Atlas
-
-// para que la placa se conecte a la red 
-const char* ssid= "Tenda_4638F8" 
-const char* password = "m6R2A6Rq"
-
-//web server    no entiendo
-StaticJsonDocument<500> doc;
-
-
-
-void setup() {
-
-  Serial.begin(115200); // no se que es esto
-  dHt.begin();
-
-    pinMode(LEDamarillopin, OUTPUT);
-    pinMode(LEDverdepin, OUTPUT);
-    pinMode(SOILPOWER, OUTPUT);
-
-    digitalwrite(LEDamarillopin , LOW);
-    digitalwrite(LEDverdepin , LOW);
-    digitalwrite(SOILPOWER , LOW);
-    //coneccion wifi
-    Wifi.begin(ssid,password);
-      Serial.println("conectandose a la red "+ ssid + " ...");
-    while(Wifi.status() != WL_CONNECTED){
-      serial.print(".");
-      delay(300);
-    }
-      Serial.println();
-      Serial.print("Connected with IP: ");
-      Serial.println(Wifi.localIP());
-      Serial.println();
-
-      configTime(0,0, ntpserver);
-}
-
-void loop() {
-  if(millis()- dataMillis > 15000 || dataMillis == 0){
-    dataMillis = millis();
-
-    epochTime = getTime();
-    Serial.print("epoch time: ");
-    Serial.println(epochTime);
-
-    float tempreatura = dHt.readTemperature();
-    float humedad = dHt.readHumidity();
-
-    digitalWrite(SOILPOWER, HIGH);
-    delay(10);
-    float moisture = analogRead(SOILPIN);
-    float moisturepercent = 100.00 - ((moisture/4095.00) * 100.00 );
-
-    digitalWrite(SOILPOWER, LOW);
-
-    float ldr =  analogRead(SOILPIN);
-    float ldrPercent = (ldr/4095.00) * 100.00;
-
-    Serial.print("temperatura: ");
-    Serial.print(String(temperatura));
-    Serial.print(" C\n humedad: ");
-    Serial.print(String(humedad));
-    Serial.print(" \n moisture: ");
-    Serial.print(String(moisturepercent));
-    Serial.print(" % ");
-    Serial.print(" \n ligth: ");
-    Serial.print(String(ldrPercent));
-    Serial.print(" % ");
-    Serial.println("\n ");
-
-    doc["sensors"]["tempreatura"] = temperatura;
-    doc["sensors"]["humedad"]     = humedad;
-    doc["sensors"]["moisture"]    = moisturepercent;
-    doc["sensors"]["ligth"]       = ldrPercent;
-    doc["sensors"]["timestamp"]   = epochTime;
-
-    Serial.println("actualizando data...");
-    POSTData();
-    
-  }
-}
-
-// extra que no entiendo 
-unsigned long getTime(){
-  time_t now;
-  struct tm timeinfo;
-  if (!getLocalTime(&timeinfo)){
-    return(0);
-  }
-  time(&now);
-  return now;
-}
-
-void POSTData(){
-  if(Wifi.status() == WL_CONNECTED){
-    HTTPClient http;
-
-    http.begin(servername);
-    http.addHeader("Content-Type", "application/Json");
-
-    String Json;
-    serializeJson(doc, Json);
-
-    Serial.println(Json);
-    int httpResponseCode = http.POST(Json);
-    Serial.println(httpResponseCode);
-
-    if(httpResponseCode == 204){
-      Serial.println("data uploaded.");
-      digitalWrite(LEDamarillopin, HIGH);
-      delay(200);
-      digitalWrite(LEDamarillopin, HIGH);
-    }else{
-      Serial.println("ERROR");
-      digitalWrite(LEDverdepin, HIGH);
-      delay(200);
-      digitalWrite(LEDverdepin, HIGH);
-    }
-
-
-  }
-}
- */
-
-
-
-
-///////////////////////////////////////////////////////////
-//codigo corregido por chatgpt
-///////////////////////////////////////////////////////////
-
-
-#include <WiFi.h>
-#include <DHT.h>
+#include <DHTesp.h>
 #include <HTTPClient.h>
 #include <ArduinoJson.h>
 #include "time.h"
 
 // Definir pines
-#define DHTPIN 27
-#define SOILPIN 32
-#define LDRPIN 33
-#define LEDAMARILLOPIN 15 // respuesta positiva
-#define LEDVERDEPIN 16    // respuesta negativa
-#define SOILPOWER 4
+#define DHTPIN 15             // Pin donde está conectado el sensor DHT11
+#define LEDAMARILLOPIN 16     // LED amarillo
+#define LEDVERDEPIN 17        // LED verde
 
-DHT dht(DHTPIN, DHT11);
+DHTesp dht;
 
 unsigned long epochtime;
 unsigned long dataMillis = 0;
 
 const char* ntpServer = "pool.ntp.org";
-const char* serverName = "mongodb+srv://gun45780:adso145780@cluster0.feqs5gd.mongodb.net/";
+const char* serverName = "https://us-east-1.aws.data.mongodb-api.com/app/data-ndbugol/endpoint/data/v1/action/insertOne";  // URL de la API de MongoDB Data
 
 // Credenciales WiFi
 const char* ssid = "Tenda_4638F8";
 const char* password = "m6R2A6Rq";
 
+// Nombre de la base de datos y colección en MongoDB Atlas
+const char* databaseName = "datos_de_arduino";
+const char* collectionName = "datos";
+
 // Documento JSON
-StaticJsonDocument<500> doc;
+StaticJsonDocument<200> doc;
 
 void setup() {
-  Serial.begin(115200); // Inicializar comunicación serial
-  dht.begin();
+  Serial.begin(9600); // Inicializar comunicación serial
+  dht.setup(DHTPIN, DHTesp::DHT11);
 
   pinMode(LEDAMARILLOPIN, OUTPUT);
   pinMode(LEDVERDEPIN, OUTPUT);
-  pinMode(SOILPOWER, OUTPUT);
 
   digitalWrite(LEDAMARILLOPIN, LOW);
   digitalWrite(LEDVERDEPIN, LOW);
-  digitalWrite(SOILPOWER, LOW);
 
   // Conexión WiFi
   WiFi.begin(ssid, password);
@@ -226,33 +61,19 @@ void loop() {
     Serial.print("Epoch time: ");
     Serial.println(epochtime);
 
-    float temperatura = dht.readTemperature();
-    float humedad = dht.readHumidity();
-
-    digitalWrite(SOILPOWER, HIGH);
-    delay(10);
-    float moisture = analogRead(SOILPIN);
-    float moisturePercent = 100.00 - ((moisture / 4095.00) * 100.00);
-
-    digitalWrite(SOILPOWER, LOW);
-
-    float ldr = analogRead(LDRPIN);
-    float ldrPercent = (ldr / 4095.00) * 100.00;
+    TempAndHumidity data = dht.getTempAndHumidity();
 
     Serial.print("Temperatura: ");
-    Serial.print(temperatura);
-    Serial.print(" C\nHumedad: ");
-    Serial.print(humedad);
-    Serial.print(" %\nMoisture: ");
-    Serial.print(moisturePercent);
-    Serial.print(" %\nLuz: ");
-    Serial.print(ldrPercent);
-    Serial.print(" %\n");
+    Serial.print(data.temperature);
+    Serial.print(" °C\nHumedad: ");
+    Serial.print(data.humidity);
+    Serial.println(" %");
 
-    doc["sensors"]["temperatura"] = temperatura;
-    doc["sensors"]["humedad"] = humedad;
-    doc["sensors"]["moisture"] = moisturePercent;
-    doc["sensors"]["luz"] = ldrPercent;
+    // Redondear temperatura a dos decimales
+    float temperaturaRedondeada = round(data.temperature * 100.0) / 100.0;
+
+    doc["sensors"]["temperatura"] = temperaturaRedondeada;
+    doc["sensors"]["humedad"] = data.humidity;
     doc["sensors"]["timestamp"] = epochtime;
 
     Serial.println("Actualizando datos...");
@@ -276,25 +97,32 @@ void POSTData() {
 
     http.begin(serverName);
     http.addHeader("Content-Type", "application/json");
+    http.addHeader("api-key", "WD0PmvqccnHxPYc4YEsOK3hryZAN6fca4glv0XLQTaMeAZID4Yh4zGZQNpiXEdsz");
 
+    // Construir el JSON con los datos
     String json;
     serializeJson(doc, json);
 
+    Serial.print("JSON a enviar: ");
     Serial.println(json);
+
     int httpResponseCode = http.POST(json);
+    Serial.print("Código de respuesta HTTP: ");
     Serial.println(httpResponseCode);
 
-    if (httpResponseCode == 204) {
-      Serial.println("Datos subidos.");
+    if (httpResponseCode == 201) { // 201 significa que se creó el recurso (documento)
+      Serial.println("Datos subidos correctamente.");
       digitalWrite(LEDAMARILLOPIN, HIGH);
       delay(200);
       digitalWrite(LEDAMARILLOPIN, LOW);
     } else {
-      Serial.println("ERROR");
+      Serial.print("Error al subir los datos. Código de respuesta: ");
+      Serial.println(httpResponseCode);
       digitalWrite(LEDVERDEPIN, HIGH);
       delay(200);
       digitalWrite(LEDVERDEPIN, LOW);
     }
+
     http.end();
   }
 }
